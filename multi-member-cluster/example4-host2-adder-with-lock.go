bec7930c56e8de21872ac04e394da8ae7fc19915
Yunkon (Alvin) Kim <hermitkim1@gmail.com>
2021-03-12 17:17:13 +0900

    Add and update etcd clients to test

--- multi-member-cluster/example4-host2-adder-with-lock.go
+++ multi-member-cluster/example4-host2-adder-with-lock.go
@@ -2,16 +2,21 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"go.etcd.io/etcd/client/v3"
+	"go.etcd.io/etcd/client/v3/concurrency"
 	"log"
 	"strconv"
 	"time"
 )
 
+
 func main() {
 
 	var myKey = "phoo"
+	var name = flag.String("name", myKey, "give a name")
+	flag.Parse()
 
 	// Adder Section
 	adderClient, err := clientv3.New(clientv3.Config{
@@ -25,6 +30,11 @@ func main() {
 
 	fmt.Println("Adder is connected.")
 
+	// Create a sessions to aqcuire a lock
+	session, _:= concurrency.NewSession(adderClient)
+	defer session.Close()
+
+	lock := concurrency.NewMutex(session, "/distributed-lock/")
 	ctx := context.Background()
 
 	// Set "phoo" with "50" initially
@@ -32,16 +42,28 @@ func main() {
 
 	// Try to add value of "phoo" 100 times
 	for i := 0; i < 100; i++ {
+		// Acquire lock (or wait to have it)
+		if err := lock.Lock(ctx); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Acquired lock for ", *name)
+
 		resp1, _ := adderClient.Get(ctx, myKey)
 		num1, _ := strconv.Atoi(string(resp1.Kvs[0].Value))
 		num1++
 		fmt.Printf("Adder: %v\n", num1)
 		adderClient.Put(ctx, myKey, strconv.Itoa(num1))
 		time.Sleep(10 * time.Millisecond)
+
+		// Release lock
+		if err := lock.Unlock(ctx); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Released lock for ", *name)
 	}
 
 	fmt.Println("Done to add")
 
 	var ch chan bool
-	<-ch // Block forever
+	<- ch // Block forever
 }
--- multi-member-cluster/example4-host3-subtractor-with-lock.go
+++ multi-member-cluster/example4-host3-subtractor-with-lock.go
@@ -2,16 +2,21 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"go.etcd.io/etcd/client/v3"
+	"go.etcd.io/etcd/client/v3/concurrency"
 	"log"
 	"strconv"
 	"time"
 )
 
+
 func main() {
 
 	var myKey = "phoo"
+	var name = flag.String("name", myKey, "Give a name")
+	flag.Parse()
 
 	// Subtractor Section
 	subtractorClient, err := clientv3.New(clientv3.Config{
@@ -25,19 +30,37 @@ func main() {
 
 	fmt.Println("The subtractor is connected.")
 
+	// Create a sessions to aqcuire a lock
+	session, _:= concurrency.NewSession(subtractorClient)
+	defer session.Close()
+
+	lock := concurrency.NewMutex(session, "/distributed-lock/")
 	ctx := context.Background()
 
 	for j := 0; j < 100; j++ {
+		// Acquire lock (or wait to have it)
+		if err := lock.Lock(ctx); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Acquired lock for ", *name)
+
 		resp2, _ := subtractorClient.Get(ctx, myKey)
 		num2, _ := strconv.Atoi(string(resp2.Kvs[0].Value))
 		num2--
 		fmt.Printf("Subtractordder: %v\n", num2)
 		subtractorClient.Put(ctx, myKey, strconv.Itoa(num2))
 		time.Sleep(10 * time.Millisecond)
+
+		// Release lock
+		if err := lock.Unlock(ctx); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Released lock for ", *name)
 	}
 
 	fmt.Println("Done to subtract")
 
+
 	var ch chan bool
-	<-ch // Block forever
+	<- ch // Block forever
 }
