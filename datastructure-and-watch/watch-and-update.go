5a12c73ec58c912fe55936e9e3b46154420a071e
Yunkon (Alvin) Kim <hermitkim1@gmail.com>
2021-10-19 15:19:24 +0900

    Add test code to check `watch` result based on key depth

---
+++ datastructure-and-watch/watch-and-update.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"time"
+
+	clientv3 "go.etcd.io/etcd/client/v3"
+	"go.etcd.io/etcd/client/v3/concurrency"
+)
+
+func main() {
+
+	// var name = flag.String("name", "foo", "Give a name")
+	flag.Parse()
+
+	// Watcher Section
+	go func() {
+		watcherClient, err := clientv3.New(clientv3.Config{
+			Endpoints:   []string{"localhost:2379"},
+			DialTimeout: 5 * time.Second,
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer watcherClient.Close()
+
+		rch := watcherClient.Watch(context.Background(), "root", clientv3.WithPrefix())
+		for wresp := range rch {
+			for _, ev := range wresp.Events {
+				fmt.Printf("Watch - %s %q : %q\n", ev.Type, ev.Kv.Key, ev.Kv.Value)
+			}
+		}
+	}()
+
+	time.Sleep(1000)
+	// Adder Section
+	updaterClient, err := clientv3.New(clientv3.Config{
+		Endpoints:   []string{"localhost:2379"},
+		DialTimeout: 5 * time.Second,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer updaterClient.Close()
+
+	// Create a sessions to aqcuire a lock
+	s1, _ := concurrency.NewSession(updaterClient)
+	defer s1.Close()
+
+	// l1 := concurrency.NewMutex(s1, "/distributed-lock/")
+	// ctx1 := context.Background()
+
+	updaterClient.Put(context.Background(), "root", "I'm root")
+	updaterClient.Put(context.Background(), "root/depth1", "I'm depth1")
+	updaterClient.Put(context.Background(), "root/depth1/depth2", "I'm depth2")
+
+	var ch chan bool
+	<-ch // Block forever
+}
